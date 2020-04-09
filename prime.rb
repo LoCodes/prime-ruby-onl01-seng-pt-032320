a5a335e4d5484f5effb663b862f05cd527b214c2
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-09 18:17:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@
 
 
 def prime? (num)# Add  code here!
-  if num < 0 #or num == 0 or num == 1
+  if num < 0 or num == 0 or num == 1
     return false
   else
     (2..num-1).to_a.all? do |possible_factor|
